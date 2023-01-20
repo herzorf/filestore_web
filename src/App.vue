22b53ee53ec09cc8d544e530233312d361db70f1
herzorf <1446450047@qq.com>
2023-01-21 00:14:45 +0800

    添加路由跳转

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>hello word</h1>
   <router-view></router-view>
+  <router-link to="/signin">signin</router-link>
+  <router-link to="/signup">signup</router-link>
+  <router-link to="/home">home</router-link>
 </template>
 
 <style scoped></style>
