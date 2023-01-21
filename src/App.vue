d17c647e633f02e029153190d633ceb7693d9ec9
herzorf <1446450047@qq.com>
2023-01-21 19:32:13 +0800

    获取用户信息接口

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="wrapper">
     <router-view></router-view>
-    <router-link to="/signin">signin</router-link>
-    <router-link to="/signup">signup</router-link>
-    <router-link to="/home">home</router-link>
+    <router-link to="/signin">signin | </router-link>
+    <router-link to="/signup">signup | </router-link>
+    <router-link to="/home">home | </router-link>
   </div>
 </template>
 
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <h1>home</h1>
+    <div>{{ userInfo }}</div>
   </div>
 </template>
-<script lang="ts" setup name="Home"></script>
+<script lang="ts" setup name="Home">
+  import { reactive, ref } from "vue";
+  import http from "../../http";
+  let username = localStorage.getItem("username") || undefined;
+  let token = localStorage.getItem("token") || undefined;
+  let userInfo = ref();
+  http({
+    url: "/api/user/info",
+    method: "post",
+    data: {
+      username,
+      token,
+    },
+  }).then((res) => {
+    userInfo.value = res.data.data;
+  });
+</script>
 <style lang=""></style>
