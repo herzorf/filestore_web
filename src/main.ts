d6a4494cc9a4e537de8da77bd5d21dcbb49cc67a
herzorf <1446450047@qq.com>
2023-01-24 23:06:28 +0800

    更改title

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import routes from './routes/routes'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './routes/routes'
